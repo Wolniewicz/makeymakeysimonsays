c439f18fcea3079bb5f7ce0bcb057a0ab5014902
kubalaj <joseph.kubala@colorado.edu>
2014-04-28 18:05:57 -0600

    here is the code testing

--- simonsayssound.py
+++ simonsayssound.py
@@ -5,6 +5,18 @@ Freq = 2500 # Set Frequency To 2500 Hertz
 Dur = 1000 # Set Duration To 1000 ms == 1 second
 winsound.Beep(Freq,Dur)
 
+ans = []
+usr = []
+#Down Arrow
+winsound.Beep(Freq,Dur)
+ans.append("a")
+#Down Arrow
+winsound.Beep(Freq,Dur)
+ans.append("a")
+#Up Arrow
+winsound.Beep(3000,Dur)
+ans.append("b")
+
 while True:
     key = ord(getch())
     if key == 27: #ESC
@@ -15,9 +27,19 @@ while True:
         key = ord(getch())
         if key == 80: #Down arrow
             winsound.Beep(Freq,Dur)
+            ans.append("a")
         elif key == 72: #Up arrow
             winsound.Beep(3000,Dur)
+            ans.append("b")
         elif key == 75: #right arrow
             winsound.Beep(3500,Dur)
+            ans.append("c")
         elif key == 77: #left arrow
             winsound.Beep(4000,Dur)
+            ans.append("d")
+
+if ans == usr:
+    print 'YOU WON!!!'
+    exit()
+
+print 'YOU LOST!!!'
